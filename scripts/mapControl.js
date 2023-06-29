794652a9514aedcd62bdf2a1715be5b1755da357
jacobm2000 <maurerja2000@gmail.com>
2023-06-28 20:02:25 -0400

    Added more content
    added more lakes to the fishing location page. Also created a scripts folder.

--- scripts/mapControl.js
+++ scripts/mapControl.js
@@ -1,6 +1,9 @@
 btnErie= document.getElementById("erie-btn")
 btnWallenpaupack= document.getElementById("wallenpaupack-btn")
 btnRaystown= document.getElementById("raystown-btn")
+btnPymatuning=document.getElementById("pymatuning-btn")
+btnHigh=document.getElementById("High-btn")
+btnArthur=document.getElementById("arthur-btn")
 map=document.getElementById("map")
 txt=document.getElementById("currLake")
 
@@ -23,11 +26,31 @@ function newMap(location){
         txt.innerHTML="Current Lake: Raystown Lake"
         map.src=src="https://www.openstreetmap.org/export/embed.html?bbox=-78.2878875732422%2C40.26590441926665%2C-77.94456481933595%2C40.45060475430765&amp;layer=mapnik"
     }
+    if (location==="High")
+    {
+        txt.innerHTML="Current Lake: High Point Lake"
+        map.src=src="https://www.openstreetmap.org/export/embed.html?bbox=-79.25777435302736%2C39.75590048332728%2C-79.1719436645508%2C39.80246983499714&amp;layer=mapnik" 
+    }
+
+    if (location==="Pymatuning")
+    {
+        txt.innerHTML="Current Lake: Pymatuning Reservoir"
+        map.src=src="https://www.openstreetmap.org/export/embed.html?bbox=-80.68084716796876%2C41.50857729743935%2C-80.33752441406251%2C41.68983501879767&amp;layer=mapnik" 
+    }
+    
+    if (location==="arthur")
+    {
+        txt.innerHTML="Current Lake: Lake Arthur"
+        map.src=src="https://www.openstreetmap.org/export/embed.html?bbox=-80.13831138610841%2C40.925640688968556%2C-80.05248069763185%2C40.9714091170842&amp;layer=mapnik" 
+    }
     
     
- 
+    
   
 }
 btnErie.addEventListener("click",newMap.bind(location,"Erie"))
 btnWallenpaupack.addEventListener("click",newMap.bind(location,"Wallenpaupack"))
 btnRaystown.addEventListener("click",newMap.bind(location,"Raystown Lake"))
+btnPymatuning.addEventListener("click",newMap.bind(location,"Pymatuning"))
+btnHigh.addEventListener("click",newMap.bind(location,"High"))
+btnArthur.addEventListener("click",newMap.bind(location,"arthur"))
